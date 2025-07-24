dd02c751fa09c11417db651ba04f8e6d5e6e8efb
xera-2011 <1138006034@qq.com>
2025-07-24 22:41:39 +0800

    Update file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,16 +9,10 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-// const eslintConfig = [
-//   ...compat.extends("next/core-web-vitals", "next/typescript"),
-//    rules: {
-//       'react/no-unescaped-entities': 'off',
-//       '@next/next/no-page-custom-font': 'off',
-//     },
-// ];
+
 const eslintConfig = [
   ...compat.config({
-    extends: ['next'],
+    extends: ["next/core-web-vitals", "next/typescript"],
     rules: {
       'react/no-unescaped-entities': 'off',
       '@next/next/no-page-custom-font': 'off',
